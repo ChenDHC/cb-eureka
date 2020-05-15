03582df8acbd9be7256db7572be650638b4c81ec
chenbin <chenbin@uzoo.cn>
2020-05-15 15:24:11 +0800

    init project

--- eureka-center/src/main/java/com/cb/eureka/center/EurekaCenterApplication.java
+++ eureka-center/src/main/java/com/cb/eureka/center/EurekaCenterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class EurekaCenterApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(EurekaCenterApplication.class, args);
 	}
 }
